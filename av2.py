18d92848381f3002a160581a254c4c6cea253b13
ERYCKLIS DUARTE <227676984+duarteerycklis@users.noreply.github.com>
2025-11-24 23:32:11 -0300

    Add files via upload

---
+++ av2.py
@@ -0,0 +1,52 @@
+# Importa o módulo socket
+from socket import *
+import sys # Necessário para encerrar o programa
+
+# Cria o socket TCP (orientado à conexão)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# Prepara o socket do servidor
+
+serverSocket.bind(('127.0.0.1', 6789))
+serverSocket.listen(1)
+
+while True:
+    # Estabelece a conexão
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+
+    try:
+        # Recebe a mensagem do cliente (requisição HTTP)
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:])
+
+        outputdata = f.read()
+    
+        # Envia a linha de status do cabeçalho HTTP
+        filename == "/"
+        filename = "/av2.html"
+        connectionSocket.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
+
+
+        # Envia o conteúdo do arquivo ao cliente
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        # Fecha a conexão com o cliente
+        connectionSocket.close()
+        
+    except IOError:
+        # Envia mensagem de erro 404 se o arquivo não for encontrado
+        connectionSocket.sendall("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        connectionSocket.sendall("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
+        
+        # Fecha a conexão com o cliente
+
+        
+        connectionSocket.close()
+
+
+    serverSocket.close()
+    sys.exit() # Encerra o programa
